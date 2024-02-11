aebb80f5be0934b003023a124818e1a7e8047523
Rohit Lodhi <151428574+rohitlodhiii@users.noreply.github.com>
2024-02-11 23:58:44 +0530

    [#4] Update Hero.jsx

--- src/components/heroSection.jsx/Hero.jsx
+++ src/components/heroSection.jsx/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="hero min-h-screen bg-base-200">
   <div className="hero-content text-center ">
     <div className="max-w-md">
-      <h1 className="text-5xl font-bold">Astra JS</h1>
+      <h1 className="text-5xl font-bold">AstraUI</h1>
       <p className="py-6">A simple , installation free ,copy paste UI components library for tailwind css JSX components.</p>
       {/* Open the modal using document.getElementById('ID').showModal() method */}
 <button className="btn btn-accent" onClick={()=>document.getElementById('my_modal_1').showModal()}>Explore</button>
